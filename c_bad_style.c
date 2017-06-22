9211647c1121167599bca47e803f943589b5ef15
scryver <scryver@users.noreply.github.com>
2017-06-22 02:08:09 +0200

    Updated bad style to make it worse

--- c_bad_style.c
+++ c_bad_style.c
@@ -29,14 +29,14 @@ struct DataPacket { int x; int y; float z;};
 struct DataPacket find_packet(int x)
 {
     int x; // a var
-    int y;
-    char z;
-    unsigned long long d2;
+int y;
+ char z;
+	    unsigned long long d2;
     const char* name;
-    int one,two,three=1;
+     int one,two, three=1;
 			if (x) {
         do_this;
-        do_that;
+         do_that;
 } else {do_that;    }
 
     do_something_different_for_every_call(with_one_item(x, y, one, two), with_second(name, x));
@@ -53,46 +53,43 @@ x = (x & 4) | 3;
         && longer_name_y == 5)
         || ((longer_name_d<4 && longer_namer_d==3)
         && (x == 1)))
-    {
-        this_is_only_to_show_long_boolean_operators();
-        nothing_else();
+    { this_is_only_to_show_long_boolean_operators();
+          nothing_else();
     }
     else if ((long_name_x == 4 || longer_name_y == 5) && ((longer_name_d<4 && longer_namer_d==3)
      && (x == 1)))
     {
-        this_is_only_to_show_long_boolean_operators();
-        nothing_else();
-    }
-    else
-    {
+           this_is_only_to_show_long_boolean_operators();
+      nothing_else();}
+    else {
         one_thing();
     }
 
     switch(x)
     {
-        case 0: process_zero();
+case 0: process_zero();
         break;
         case 1:
             do_one_time();
-            break;
-        case 2: {
+             break;
+			case 2: {
             int some_new_var;   // use braces for cases that define new variables
             do_two_times(some_new_var);
             break;
         }
-        default: break;
+         default: break;
     }
   return packets[x];
 }
 
 #ifdef FIRST_TEST_DEFINE
-for (int i=0; i < max_iter; ++i) {
-    process_data(i * max_iter);
+for (int i=0;i< max_iter; ++i) {
+      process_data(i * max_iter);
     process_more();
 }
 #elif SECOND_DEFINE         // it is not FIRST_TEST_DEFINE
-for (int i=0; i < max_iter; ++i)
+for (int i =0; i <max_iter; ++i)
     one_line_processing(i * max_iter);
 #else // it is not FIRST_TEST_DEFINE or SECOND_DEFINE
-for (int i=0;i<max_iter;++i); //waiter
+for (int i= 0;i<max_iter;++i); //waiter
 #endif // FIRST_TEST_DEFINE or SECOND_DEFINE
